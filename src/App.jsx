09f92ed8169a577b8896bfbf2f0ac988da0c5e0a
Aman Wadhwaniya <amanwadhwaniya159@gmail.com>
2023-02-18 12:02:19 +0530

    Login Added!

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,22 @@ import Home from "./pages/Home";
 import Pen from "./pages/pen";
 import NotFound from "./pages/NotFound";
 import Signup from "./pages/accounts/Signup";
+import ForgotPassword from "./pages/accounts/Forgot";
 import Login from "./pages/accounts/Login";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/pen" element={<Pen />} />
-      <Route path="/accounts/signup" element={<Signup />} />
-      <Route path="/accounts/login" element={<Login />} />
-      <Route path="*" element={<NotFound />} />
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/pen" element={<Pen />} />
+      <Route exact path="/accounts/signup" element={<Signup />} />
+      <Route exact path="/accounts/login" element={<Login />} />
+      <Route
+        exact
+        path="/accounts/forgot-password"
+        element={<ForgotPassword />}
+      />
+      <Route exact path="*" element={<NotFound />} />
     </Routes>
   );
 }
