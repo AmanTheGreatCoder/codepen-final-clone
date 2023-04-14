c9484fb4a3177b3664cbd06edf51de76154aecca
AmanTheGreatCoder <amanwadhwaniya159@gmail.com>
2023-04-14 17:41:52 +0530

    Added Form CSS!

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import "./index2.css"
+import "./form.css"
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
