083f01ecbf40e7723e3f5c195dcccd39e2d02f99
AmanTheGreatCoder <amanwadhwaniya159@gmail.com>
2023-04-14 18:14:01 +0530

    Where is darkreader style comming from

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import "./index2.css"
-import "./form.css"
-import "./homepage.css"
-import "./everypage.css"
+// import "./index.css";
+// import "./index2.css"
+// import "./form.css"
+// import "./homepage.css"
+// import "./everypage.css"
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
